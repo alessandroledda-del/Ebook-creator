c70771fe0586136ccc104b5ca7cc93bbe146e08d
Alessandro Ledda <alessandro.ledda@tempocasa.com>
2026-03-31 12:58:20 +0200

    Refactor Book model to use Mongoose schema

--- models/book.js
+++ models/book.js
@@ -1,1 +1,20 @@
-class Book { constructor(title, author) { this.title = title; this.author = author; } } module.exports = Book;
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    titolo: { type: String, required: true },
+    autore: { type: String, required: true },
+    descrizione: { type: String, default: '' },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    statoCreazione: { type: String, enum: ['bozza', 'in-revisione', 'pubblicato'], default: 'bozza' },
+    genere: { type: String, default: '' },
+    tag: [String],
+    dataCrazione: { type: Date, default: Date.now },
+    dataModifica: { type: Date, default: Date.now }
+});
+
+bookSchema.pre('save', function(next) {
+    this.dataModifica = Date.now();
+    next();
+});
+
+module.exports = mongoose.model('Book', bookSchema);
